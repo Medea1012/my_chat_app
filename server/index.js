71d61bbd5471a774e06fa8b13a28e455b3369b28
Medea Wang <medea.wangxy@gmail.com>
2023-09-03 22:20:31 -0400

    milestone0

--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("./Routes/userRoute");
 
 const app = express();
+require("dotenv").config();
+
 app.use(express.json());
 app.use(cors());
+app.use("/api/users", userRoute);
 
-const port = 5000;
+app.get("/", (req, res) => {
+  res.send("Welcome our chat app APIs..");
+});
+
+const port = process.env.PORT;
+const uri = process.env.ATLAS_URI;
 
 app.listen(port, (req, res) => {
   console.log(`Server running on port: ${port}`);
 });
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection established"))
+  .catch((error) => console.log("MongoDB connection fail:", error.message));
